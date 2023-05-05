0a3871c11da8bbdb72e379ac3cc4b4c487c0de4f
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-05 15:16:01 +0530

    Added: valid first name

--- src/com/bridgelabz/RegEx.java
+++ src/com/bridgelabz/RegEx.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 public class RegEx {
     public static void main(String[] args) {
-        System.out.println("Welcome to RegEx Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter First Name :");
+        String string = scanner.next();
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher matcher = pattern.matcher(string);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Invalid");
+        }
     }
 }
